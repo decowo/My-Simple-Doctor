bdbb0b744209e91f22403effcc8aa37a25bb14de
Decowo <joeljunior933@gmail.com>
2021-03-22 20:33:07 -0300

    Postgree_FK

--- Doctor-master/src/main/java/com/example/PA4/Entidade/RatingsFarm.java
+++ Doctor-master/src/main/java/com/example/PA4/Entidade/RatingsFarm.java
@@ -1,8 +1,12 @@
 package com.example.PA4.Entidade;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 
@@ -10,19 +14,21 @@ import javax.persistence.Table;
 @Table(name="TB_RatingFarm")
 public class RatingsFarm {
     private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int idFarm_2;
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+    private Farmacia idFarm_2;
     private float RatingFarm;
     private int idFarmnota; //saber qual idfarm vai receber a nota
-    private int iduser; //fk
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private Usuario iduser; //fk
     public static long getSerialversionuid() {
         return serialVersionUID;
     }
-    public int getIdFarm_2() {
+    public Farmacia getIdFarm_2() {
         return idFarm_2;
     }
-    public void setIdFarm_2(int idFarm_2) {
+    public void setIdFarm_2(Farmacia idFarm_2) {
         this.idFarm_2 = idFarm_2;
     }
     public float getRatingFarm() {
@@ -37,10 +43,10 @@ public class RatingsFarm {
     public void setIdFarmnota(int idFarmnota) {
         this.idFarmnota = idFarmnota;
     }
-    public int getIduser() {
+    public Usuario getIduser() {
         return iduser;
     }
-    public void setIduser(int iduser) {
+    public void setIduser(Usuario iduser) {
         this.iduser = iduser;
     }
 }
--- Doctor-master/src/main/java/com/example/PA4/Entidade/RatingsMed.java
+++ Doctor-master/src/main/java/com/example/PA4/Entidade/RatingsMed.java
@@ -1,31 +1,33 @@
 package com.example.PA4.Entidade;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="TB_RatingMedicamento")
 public class RatingsMed {
     private static final long serialVersionUID = 1L;
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int idmed_2;
+    private Medicamento idmed_2;
     private float Rating;
     private int idmednota; //saber qual idmed vai receber a nota
-    //@OneToOne
-    //@JoinColumn(name="id")
-    private int iduser; //fk
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private Usuario iduser; //fk
     public static long getSerialversionuid() {
         return serialVersionUID;
     }
-    public int getIdmed_2() {
+    public Medicamento getIdmed_2() {
         return idmed_2;
     }
-    public void setIdmed_2(int idmed_2) {
+    public void setIdmed_2(Medicamento idmed_2) {
         this.idmed_2 = idmed_2;
     }
     public float getRating() {
@@ -40,10 +42,10 @@ public class RatingsMed {
     public void setIdmednota(int idmednota) {
         this.idmednota = idmednota;
     }
-    public int getIduser() {
+    public Usuario getIduser() {
         return iduser;
     }
-    public void setIduser(int iduser) {
+    public void setIduser(Usuario iduser) {
         this.iduser = iduser;
     }
 }
